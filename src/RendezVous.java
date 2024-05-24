34954e3023a80f2aa656febc72467779719f3b1e
alili-khadija <lk_alili@esi.dz>
2024-05-24 02:02:37 +0100

    methods

---
+++ src/RendezVous.java
@@ -0,0 +1,10 @@
+import java.sql.Time;
+import java.util.Date;
+
+public abstract class RendezVous {
+    private Date date;
+    private Time time;
+    private Time duree;
+    private String observation;
+    private TypeRV typeRV;
+}
